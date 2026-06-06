42f7b75c22ddb5e615c9a3441493ef1fa20e52ad
ZhairMG <maldonzh@oregonstate.edu>
2026-06-06 15:40:04 -0700

    Added more comments

--- or.sv
+++ or.sv
@@ -1,3 +1,12 @@
+/*
+Author: David Smith
+Description: Takes in two inputs, A and B
+compare them and if the pair of bits has at least 1
+1, return 1, otherwise return 0
+
+sources used: textbook, class examples
+
+*/
 module my_or8 (
   input logic [7:0] a, b,
   output logic [7:0] result
--- xor.sv
+++ xor.sv
@@ -1,8 +1,17 @@
+/*
+Author: David Smith
+Description: gets an input from A and B,
+compares the bits and if only one of the bit pairs
+is 1 then return 1, otherwise return 0
+
+Sources Used:Textbook,
+*/
+
 module my_xor8 (
   input logic [7:0] a, b,
   output logic [7:0] result
 );
-
+//compares the two 
 assign result = a ^ b;
 
 endmodule
